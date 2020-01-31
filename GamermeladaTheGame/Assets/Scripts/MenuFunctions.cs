582bae48dc004e9442b7b927aab9badcd347f39f
Neubey <rubendelbo@gmail.com>
2020-01-31 20:47:31 +0100

    menu scene and some functionality

---
+++ GamermeladaTheGame/Assets/Scripts/MenuFunctions.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuFunctions : MonoBehaviour
+{
+    public GameObject main_menu;
+    public GameObject options;
+    public GameObject resolutions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void InitLevel()
+    {
+
+    }
+
+    public void Options()
+    {
+        main_menu.SetActive(false);
+        options.SetActive(true);
+
+    }
+
+    public void exit_options()
+    {
+        main_menu.SetActive(true);
+        options.SetActive(false);
+    }
+
+    public void res_menu()
+    {
+        options.SetActive(false);
+        resolutions.SetActive(true);
+
+    }
+
+    public void exit_res()
+    {
+        options.SetActive(true);
+        resolutions.SetActive(false);
+    }
+
+    public void fullcreen(bool value)
+    {
+        Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, value);
+    }
+
+    public void ten_eighty()
+    {
+        Screen.SetResolution(1920, 1080, false);
+    }
+
+    public void seven_twenty()
+    {
+        Screen.SetResolution(1080, 720, false);
+    }
+
+    public void four_eighty()
+    {
+        Screen.SetResolution(640, 480, false);
+    }
+
+    public void ChangeVolume()
+    {
+
+    }
+}
