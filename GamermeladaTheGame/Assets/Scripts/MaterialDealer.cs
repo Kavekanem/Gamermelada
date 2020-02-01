6c6ccc0d66e2d020721f2d03b5b75d349bba7ff6
Neubey <rubendelbo@gmail.com>
2020-02-01 12:55:13 +0100

    los dealers van y los items te siguen to guay

--- GamermeladaTheGame/Assets/Scripts/MaterialDealer.cs
+++ GamermeladaTheGame/Assets/Scripts/MaterialDealer.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class MaterialDealer : MonoBehaviour
 {
     public GameObject material_to_deal;
-
+    [HideInInspector]
+    public bool deal_own = false;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +16,25 @@ public class MaterialDealer : MonoBehaviour
 
     public GameObject getItsMaterial()
     {
-        return Object.Instantiate(material_to_deal, transform.position, material_to_deal.transform.rotation);
+        if (deal_own)
+            return this.gameObject;
+        else
+            return Object.Instantiate(material_to_deal, transform.position, material_to_deal.transform.rotation);
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("Collided");
+        StoringItems component = collision.gameObject.GetComponent<StoringItems>();
+        if (component.object_counter < component.max_carryin_items && component != null)
+        {
+            Debug.Log("Collided2");
+
+            component.carrying_objects[component.object_counter] = getItsMaterial();
+            if(component.object_counter != 0)
+                component.carrying_objects[component.object_counter].transform.position = component.carrying_objects[component.object_counter - 1].transform.position- component.carrying_objects[component.object_counter-1].transform.forward;
+            component.object_counter++;
+        }
     }
 
     // Update is called once per frame
