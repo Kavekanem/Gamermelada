915ecac35d8a1daa10837b1dcba25bbcfde48bc5
Neubey <rubendelbo@gmail.com>
2020-02-01 14:47:28 +0100

    nyeh some fixes

--- GamermeladaTheGame/Assets/Scripts/MaterialDealer.cs
+++ GamermeladaTheGame/Assets/Scripts/MaterialDealer.cs
@@ -28,8 +28,11 @@ public class MaterialDealer : MonoBehaviour
         if (component.object_counter < component.max_carryin_items && component != null)
         {
             component.carrying_objects[component.object_counter] = getItsMaterial();
-            if(component.object_counter != 0)
-                component.carrying_objects[component.object_counter].transform.position = component.carrying_objects[component.object_counter - 1].transform.position- component.carrying_objects[component.object_counter-1].transform.forward;
+            if (component.object_counter != 0)
+                component.carrying_objects[component.object_counter].transform.position = component.carrying_objects[component.object_counter - 1].transform.position - component.carrying_objects[component.object_counter - 1].transform.forward;
+            else
+                component.carrying_objects[component.object_counter].transform.position = component.gameObject.transform.position + component.gameObject.transform.forward;
+
             component.object_counter++;
         }
     }
--- GamermeladaTheGame/Assets/Scripts/StoringItems.cs
+++ GamermeladaTheGame/Assets/Scripts/StoringItems.cs
@@ -14,10 +14,13 @@ public class StoringItems : MonoBehaviour
     public int object_counter = 0;
     public float extra_distance = 1.0f;
 
+    Rigidbody own_rb;
     // Start is called before the first frame update
     void Start()
     {
         carrying_objects = new GameObject[max_carryin_items];
+        own_rb = gameObject.GetComponent<Rigidbody>();
+
     }
 
 
@@ -40,18 +43,38 @@ public class StoringItems : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(own_rb.velocity.magnitude < 150.0f)
+        {
+
+            if(Input.GetKeyDown("w"))
+            {
+                own_rb.velocity += new Vector3(0f, 0f, -180.0f);
+            }
+            if (Input.GetKeyDown("a"))
+            {
+                own_rb.velocity += new Vector3(180.0f, 0f, 0f);
+            }
+            else if(Input.GetKeyDown("d"))
+            {
+                own_rb.velocity += new Vector3(-180.0f, 0f, 0f);
+
+            }
+        }
         //transform.position -= Vector3.Scale(transform.forward, new Vector3(1.0f, 1.0f, 1.0f));
         if (object_counter == 0)
             return;
-
-        carrying_objects[0].transform.position = transform.position + Vector3.Scale(transform.forward, (transform.lossyScale + carrying_objects[0].transform.lossyScale) * 0.5f* extra_distance);
+        Vector3 vec1 = transform.position - carrying_objects[0].transform.position;
+        vec1.Normalize();
+        carrying_objects[0].transform.position = transform.position - Vector3.Scale(vec1, (transform.lossyScale + carrying_objects[0].transform.lossyScale) * 0.5f* extra_distance);
         for(int i = 1; i < object_counter; ++i)
         {
             Vector3 vec = carrying_objects[i - 1].transform.position - carrying_objects[i].transform.position;
             vec.Normalize();
             carrying_objects[i].transform.position = carrying_objects[i-1].transform.position - Vector3.Scale(vec, (transform.lossyScale + carrying_objects[0].transform.lossyScale) * 0.5f*extra_distance);
             Rigidbody r = carrying_objects[i].GetComponent<Rigidbody>();
-          //  r.velocity = r.velocity + vec;
+
+            if(r.velocity.magnitude < own_rb.velocity.magnitude)
+                r.velocity = r.velocity + Vector3.Scale(vec,own_rb.velocity);
         }
     }
 
