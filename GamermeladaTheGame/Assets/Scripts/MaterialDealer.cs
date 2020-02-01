dc0548775626bc813977782e3812e38cfb55438b
Neubey <rubendelbo@gmail.com>
2020-02-01 23:06:52 +0100

    with spring joints, meh

--- GamermeladaTheGame/Assets/Scripts/MaterialDealer.cs
+++ GamermeladaTheGame/Assets/Scripts/MaterialDealer.cs
@@ -19,21 +19,33 @@ public class MaterialDealer : MonoBehaviour
         if (deal_own)
             return this.gameObject;
         else
-            return Object.Instantiate(material_to_deal, transform.position, material_to_deal.transform.rotation);
+        {
+            GameObject obj = Object.Instantiate(material_to_deal, transform.position, material_to_deal.transform.rotation);
+            obj.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+            Collider rb = obj.GetComponent<Collider>();
+            //rb.isTrigger = false;
+            return obj;
+        }
     }
 
     void OnTriggerEnter(Collider collision)
     {
         StoringItems component = collision.gameObject.GetComponent<StoringItems>();
-        if (component.object_counter < component.max_carryin_items && component != null)
+        if (component != null && component.object_counter < component.max_carryin_items)
         {
             component.carrying_objects[component.object_counter] = getItsMaterial();
             if (component.object_counter != 0)
+            {
                 component.carrying_objects[component.object_counter].transform.position = component.carrying_objects[component.object_counter - 1].transform.position - component.carrying_objects[component.object_counter - 1].transform.forward;
+                component.carrying_objects[component.object_counter].GetComponent<SpringJoint>().connectedBody = component.carrying_objects[component.object_counter-1].GetComponent<Rigidbody>();
+            }
             else
-                component.carrying_objects[component.object_counter].transform.position = component.gameObject.transform.position + component.gameObject.transform.forward;
+            {
+                component.carrying_objects[component.object_counter].transform.position = component.gameObject.transform.position - component.gameObject.transform.forward;
+                component.carrying_objects[component.object_counter].GetComponent<SpringJoint>().connectedBody = component.own_rb;
+            }
 
-            component.prev_pos[component.object_counter] = component.carrying_objects[component.object_counter].transform.position;
+
             component.object_counter++;
         }
     }
