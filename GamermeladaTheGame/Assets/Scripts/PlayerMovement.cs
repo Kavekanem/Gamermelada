5e275111b31f3ee2ae2b1ccacbbf21a8f26d77c1
Jorge <jorgegork@gmail.com>
2020-02-01 13:03:00 +0100

    in progress

---
+++ GamermeladaTheGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject Mesh = null;
+    public GameObject Camera = null;
+
+    private Vector3 MeshInitialOffset;
+    private Vector3 CameraInitialOffset;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
