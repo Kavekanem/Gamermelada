fba962012e09e917261945bfc9b687845c6e850b
Jorge <jorgegork@gmail.com>
2020-02-01 22:34:19 +0100

    Further iteration on level and player movement

---
+++ GamermeladaTheGame/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject Player = null;
+    public float Speed = 1.0f;
+
+    void Update()
+    {
+        Vector3 Direction = (Player.transform.position - transform.position).normalized;
+        Direction = new Vector3(Direction.x, 0.0f, Direction.z);
+
+        transform.position += Direction * Vector3.Distance(transform.position, Player.transform.position) * Speed;
+    }
+}
