39c9096a1f09a81a85b0d6885f7fad929c3142b9
Jorge <jorgegork@gmail.com>
2020-02-02 12:23:31 +0100

    Chest indicator for player

---
+++ GamermeladaTheGame/Assets/IndicatorController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var allChestComp = GameObject.FindObjectsOfType<MaterialDealer>();
+
+        //float mindistChest = 99999.0f;
+        //GameObject closestChest = null;
+
+        float mindistChest = Vector3.Distance(transform.position, allChestComp[0].gameObject.transform.position);
+        GameObject closestChest = allChestComp[0].gameObject;
+
+        for (int i = 1; i < allChestComp.Length; i++)
+        {
+            float newDist = Vector3.Distance(transform.position, allChestComp[i].gameObject.transform.position);
+
+            if(newDist < mindistChest)
+            {
+                mindistChest = newDist;
+                closestChest = allChestComp[i].gameObject;
+            }
+        }
+
+        transform.LookAt(closestChest.transform.position, new Vector3(0.0f, 1.0f, 0.0f));
+    }
+}
