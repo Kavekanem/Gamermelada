1bbb1c08779e90610c434bd56c37ff6d4ade61b2
Neubey <rubendelbo@gmail.com>
2020-02-02 12:42:41 +0100

    el holandes errante esta en nuestra costa, entregamos materiales y se reinicia

--- GamermeladaTheGame/Assets/Scripts/Pirates.cs
+++ GamermeladaTheGame/Assets/Scripts/Pirates.cs
@@ -8,34 +8,82 @@ public class Pirates : MonoBehaviour
     public enum MATERIALS { PADEL = 0, SHOVEL = 1, SWORD = 2, CHEST = 3, CANNON = 4, RUM =5};
 
     MATERIALS[] asked;
-    int objects_left = 0;
 
-    public MeshFilter boat_correct;
+    public Mesh boat_correct;
+    public Mesh boat_destroyed;
+    public CapsuleCollider collision;
+    public Collider collide;
+
+
+    public float sink_speed = 1.0f;
+
+    float rotation = 30.0f;
+    public float flying_dutchman_sink_time = 3.0f;
+    public float flying_dutchman_float_time = 3.0f;
+
+    float counter_sink = 0f;
+    float counter_float = 0f;
+
+    public int item_quantity = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        item_quantity = Random.Range(1, 6);
+
     }
 
-    public void OnTriggerEvent(Collider collider)
+    void ReloadMaterials()
     {
-        StoringItems storboat = collider.gameObject.GetComponent<StoringItems>();
-        if(storboat != null)
-        {
-            storboat.object_counter = 0;
+        MeshFilter mf = gameObject.GetComponent<MeshFilter>();
 
+        mf.mesh = boat_correct;
+        collision.enabled = false;
+
+        if (counter_sink < flying_dutchman_sink_time)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - sink_speed * Time.deltaTime, transform.position.z);
+            counter_sink += Time.deltaTime;
+            return;
+        }
+
+        mf.mesh = boat_destroyed;
+
+        if (counter_float < flying_dutchman_float_time)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + sink_speed * Time.deltaTime, transform.position.z);
+            counter_float += Time.deltaTime;
+            return;
+        }
+
+        item_quantity = Random.Range(1, 6);
+        collision.enabled = true;
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Boat blocked");
+        if (collider == collide)
+        {
+            StoringItems storboat = collider.gameObject.GetComponent<StoringItems>();
+            item_quantity -= storboat.object_counter;
+            for (int i = 0; i < storboat.object_counter; ++i)
+            {
+                GameObject.Destroy(storboat.carrying_objects[i]);
+            }
+
+            storboat.object_counter = 0;
+            counter_sink = 0f;
+            counter_float = 0f;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(objects_left == 0)
+        if(item_quantity <= 0)
         {
-            MeshFilter render = gameObject.GetComponent<MeshFilter>();
-            render = boat_correct;
-            return;
+            ReloadMaterials();
         }
     }
 }
